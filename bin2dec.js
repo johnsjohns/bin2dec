a09e6823e7836ceb2827f9ab6a75793c4e00ba28
João Figueira <53949000+johnsjohns@users.noreply.github.com>
2020-07-11 18:39:36 -0300

    Add files via upload

---
+++ bin2dec.js
@@ -0,0 +1,21 @@
+function start() {
+    let entrada = document.getElementById("texto");
+    entrada.addEventListener("keyup", verifica, false)
+}
+
+function verifica() {
+    a = (document.getElementById("texto").value);
+    for (var x = 0; x < a.length; x++) {
+        if (a[x] != 0 && a[x] != 1) {
+            alert("Numeros binario so podem ser 0 ou 1 " + x + " " + a[x]);
+            a = a.replace(a[x], "");
+            document.getElementById("texto").value = a;
+        }
+    }
+
+    numero = parseInt(a, 2);
+    if (isNaN(numero)) {
+        document.getElementById("resposta").innerHTML = "";
+    } else { document.getElementById("resposta").innerHTML = numero; }
+
+}
