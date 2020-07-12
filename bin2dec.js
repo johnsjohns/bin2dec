0d4c49839a4b139956c7868a5c211eb95a8595fa
João Figueira <53949000+johnsjohns@users.noreply.github.com>
2020-07-11 21:00:35 -0300

    Add files via upload

--- bin2dec.js
+++ bin2dec.js
@@ -1,6 +1,6 @@
 function start() {
     let entrada = document.getElementById("texto");
-    entrada.addEventListener("keyup", verifica, false);
+    entrada.addEventListener("keyup", verifica, false)
     entrada.focus();
 }
 
@@ -8,7 +8,7 @@ function verifica() {
     a = (document.getElementById("texto").value);
     for (var x = 0; x < a.length; x++) {
         if (a[x] != 0 && a[x] != 1) {
-            alert("Numeros binario so podem ser 0 ou 1 ");
+            alert("Numeros binario só podem conter 0 ou 1");
             a = a.replace(a[x], "");
             document.getElementById("texto").value = a;
         }
